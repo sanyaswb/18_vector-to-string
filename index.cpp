8561f880236915ffe95b752b2a000dba15606adc
John Doe <SlySpellar@gmail.com>
2022-10-15 11:33:11 +0300

    removed solution

--- index.cpp
+++ index.cpp
@@ -5,35 +5,5 @@
 #include <algorithm>
 
 long long int vectorToInt(std::vector<int> &numbers) {
-	long long res = 0;
-
-	int vectorSize = numbers.size();
-
-	int reversedIndex = vectorSize - 1;
-
-	for (int i = 0; i < vectorSize; i++) {
-		if (i == 0) {
-			res += numbers[reversedIndex];
-		} else {
-			int n = numbers[reversedIndex];
-
-			for (int j = 0; j < vectorSize - reversedIndex - 1; j++) {
-				n *= 10;
-			}
-
-			res += n;
-		}
-
-		reversedIndex--;
-	}
-
-	// --
-
-	std::string str;
-
-	for (int n: numbers) {
-		str += std::to_string(n);
-	}
-
-	return std::stoi(str);
+	return 0;
 }
